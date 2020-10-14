07f15dffa5f097239a9819f8f97cd70dd25e67a5
kpaluch26 <kamil.paluch98@gmail.com>
2020-10-14 11:09:36 +0200

    Pierwszy commit

---
+++ Serwer/Serwer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Serwer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
