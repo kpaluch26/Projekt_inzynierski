4d536e7ed4b642a52f01d8aa92ce245dd51c7794
kpaluch26 <kamil.paluch98@gmail.com>
2020-10-15 12:26:11 +0200

    Ponowne dodanie ręcznej konfiguracji

---
+++ Projekt_dyplomowy/Serwer/Classes/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Serwer
+{
+    class Config
+    {
+        string username;
+        string hostname;
+        string ip_address;
+        int port;
+        int buffer_size;
+
+        public Config(string u, string h, string i, int p, int b)
+        {
+            this.username = u;
+            this.ip_address = i;
+            this.port = p;
+            this.buffer_size = b;
+            this.hostname = h;  
+        }
+
+        public override string ToString()
+        {
+            return $" Użytkownik: {username} || Port: {port} || Rozmiar bufera: {buffer_size} ";
+        }
+    }
+}
---
+++ Projekt_dyplomowy/Serwer/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwer
+{
+    class Program
+    {
+        private static Config config;
+
+        static void Main(string[] args)
+        {
+        }
+
+        private static void SetConfig()
+        {
+            string username = Environment.UserName; //odczyt nazwy konta użytkownika
+            string hostName = Dns.GetHostName(); //odczyt hostname
+            string ip_address = Dns.GetHostByName(hostName).AddressList[1].ToString(); // odczyt adresu IPv4
+            int port = 0, buffer_size = 0;
+            bool next = false; //pomocnicza flaga do sprawdzania poprawności formatu
+
+            do
+            {
+                Console.WriteLine("Witaj użytkowniku " + username);
+                Console.Write("Proszę podaj port, po którym odbywać się będzię komunikacja: ");
+                try
+                {
+                    port = Convert.ToInt32(Console.ReadLine()); //ręczne ustawienie portu komunikacji
+                    next = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Niepoprawny numer portu!");
+                    Console.ReadKey(); //czekanie na potwierdzenie błedu
+                    Console.Clear();
+                    next = true;
+                }
+            } while (next);
+
+            do
+            {
+                Console.Write("Proszę podaj rozmiar buforowania: ");
+                try
+                {
+                    buffer_size = Convert.ToInt32(Console.ReadLine()); //ręczne ustawienie rozmiaru bufera
+                    next = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Niepoprawny rozmiar bufera!");
+                    Console.ReadKey(); //czekanie na potwierdzenie błedu
+                    Console.Clear();
+                    Console.WriteLine("Witaj użytkowniku " + username + "!");
+                    Console.WriteLine("Proszę podaj port, po którym odbywać się będzię komunikacja: " + port);
+                    next = true;
+                }
+            } while (next);
+
+            config = new Config(username, hostName, ip_address, port, buffer_size);//utworzenie configa
+            Console.WriteLine("Poprawna konfiguracja serwera.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void InfoDisplay() //ramka do wyświetlania informacji o konfiguracji użytkownika
+        {
+            for (int i = 0; i < config.ToString().Length + 2; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            Console.WriteLine("*" + config + "*");
+            for (int i = 0; i < config.ToString().Length + 2; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
