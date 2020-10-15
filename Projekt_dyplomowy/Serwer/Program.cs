10ee754b6898e7cacf82d7fc377e5043b593a51b
kpaluch26 <kamil.paluch98@gmail.com>
2020-10-15 13:28:35 +0200

    Rozpoczęcie pracy nad automatyczną konfiguracją

--- Projekt_dyplomowy/Serwer/Program.cs
+++ Projekt_dyplomowy/Serwer/Program.cs
@@ -4,28 +4,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Serwer
 {
-    class Program
+    class Program :Form
     {
         private static Config config;
 
+        [STAThread]
         static void Main(string[] args)
         {
+            Menu();
+            //InfoDisplay();
+        }
+
+        private static void Menu()
+        {
+            bool flags = false;
+            string caseSwitch="";
+
+                if (config == null)
+                {
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Witaj użytkowniku " + Environment.UserName);
+                            Console.WriteLine("Wybierz spoósb konfiguracji serwera:");
+                            Console.Write("1) automatyczny z pliku .txt/.xml   2) ręczna konfiguracja");
+                            ConsoleKeyInfo cki;
+                            cki = Console.ReadKey();
+                            caseSwitch = (cki.Key.ToString());
+                            flags = false;
+
+                            switch (caseSwitch)
+                            {
+                                case "D1":
+                                    Console.Clear();
+                                    ImportConfig();
+                                    break;
+                                case "D2":
+                                    Console.Clear();
+                                    SetConfig();
+                                    break;
+                                default:
+                                    throw new Exception();
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Nie ma takiej opcji, proszę wybrać poprawną opcję.");
+                            Console.Read();
+                            Console.Clear();
+                            flags = true;
+
+                        }
+                    } while (flags);
+                }
+                else
+                {
+                InfoDisplay();
+                }
         }
 
         private static void SetConfig()
         {
             string username = Environment.UserName; //odczyt nazwy konta użytkownika
             string hostName = Dns.GetHostName(); //odczyt hostname
-            string ip_address = Dns.GetHostByName(hostName).AddressList[1].ToString(); // odczyt adresu IPv4
+            string ip_address = Dns.GetHostByName(hostName).AddressList[0].ToString(); // odczyt adresu IPv4
             int port = 0, buffer_size = 0;
             bool next = false; //pomocnicza flaga do sprawdzania poprawności formatu
 
             do
             {
-                Console.WriteLine("Witaj użytkowniku " + username);
+                Console.WriteLine("Ręczna konfiguracja serwera.");
                 Console.Write("Proszę podaj port, po którym odbywać się będzię komunikacja: ");
                 try
                 {
@@ -62,6 +115,7 @@ namespace Serwer
 
             config = new Config(username, hostName, ip_address, port, buffer_size);//utworzenie configa
             Console.WriteLine("Poprawna konfiguracja serwera.");
+            //Console.WriteLine(ip_address);
             Console.ReadKey();
             Console.Clear();
         }
@@ -79,6 +133,16 @@ namespace Serwer
                 Console.Write("*");
             }
             Console.WriteLine();
+        }
+
+        private static void ImportConfig()
+        {
+            string filePath;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*.txt)|*.txt|xml files (*.xml)|*.xml";
+            ofd.ShowDialog();
+            filePath = ofd.FileName;
         }
 
     }
