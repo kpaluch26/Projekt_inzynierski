6a34b55f2de85ba1e142e294f71a84ba7c253bcd
kpaluch26 <kamil.paluch98@gmail.com>
2020-10-21 12:20:08 +0200

    Dodanie ścieżki zapisu archiwów do pliku konfiguracyjnego

--- Projekt_dyplomowy/Serwer/Config.cs
+++ Projekt_dyplomowy/Serwer/Config.cs
@@ -12,16 +12,18 @@ namespace Serwer
         private string username;
         private string hostname;
         private string ip_address;
+        private string archive_address;
         private int port;
         private int buffer_size;
 
-        public Config(string u, string h, string i, int p, int b)
+        public Config(string u, string h, string i, string a, int p, int b)
         {
             this.username = u;
             this.ip_address = i;
             this.port = p;
             this.buffer_size = b;
-            this.hostname = h;  
+            this.hostname = h;
+            this.archive_address = a;
         }
 
         public int GetPort()
@@ -34,9 +36,14 @@ namespace Serwer
             return this.buffer_size;
         }
 
+        public string GetArchiveAddress()
+        {
+            return this.archive_address;
+        }
+
         public override string ToString()
         {
-            return $" Użytkownik: {username} || AdresIPv4: {ip_address} || Port: {port} || Rozmiar bufera: {buffer_size} || Hostname: {hostname}";
+            return $" Użytkownik: {username} || AdresIPv4: {ip_address} || Hostname: {hostname} ||  Port: {port} || Rozmiar bufera: {buffer_size} || Ścieżka: {archive_address} ";
         }
     }
 }
