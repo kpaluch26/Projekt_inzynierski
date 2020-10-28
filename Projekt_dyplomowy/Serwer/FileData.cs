f3e39ce38886bf3338b6cb5c2ce6d8bf62c9caca
kpaluch26 <kamil.paluch98@gmail.com>
2020-10-28 11:28:23 +0100

    Dodanie klasy FileData

---
+++ Projekt_dyplomowy/Serwer/FileData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwer
+{
+    class FileData 
+    {
+        private string file_name;
+        private string file_address;
+        public FileData(string fn, string fa)
+        {
+            this.file_name = fn;
+            this.file_address = fa;
+        }
+
+        public string GetName()
+        {
+            return this.file_name;
+        }
+
+        public string GetAddress()
+        {
+            return this.file_address;
+        }
+
+    }
+}
